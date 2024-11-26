97ca49cb7db5cd629428743f7cd1095a6ae9df1e
Manny Ozowalu <15199323+ChukwumaOzowalu@users.noreply.github.com>
2024-11-26 16:07:12 -0600

    added inspection code

---
+++ scripts/inspection.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+# Load dataset
+file_path = "data/Trimmed_Telco_Raw.csv"
+df = pd.read_csv(file_path)
+
+# Inspect the data
+print("\n", "Dataset Shape: ", df.shape, "\n")
+print("\n", "Column information: ", df.info(), "\n")
+print("\n", "Missing Values: ", df.isnull().sum(), "\n")
+print("\n", "First Few Rows: ", df.head(5), "\n")
